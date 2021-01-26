cbd28e05857144f6b1463cd44af4c18f8393100e
NOH <scnoh02@naver.com>
2021-01-26 23:02:20 +0900

    Add Validation Logic

--- app/src/main/java/com/example/layout_practice/MainActivity.java
+++ app/src/main/java/com/example/layout_practice/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -12,8 +16,12 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextInputEditText TextInputEditText_emil, TextInputEditText_password;
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
     TextView TextView_login;
+    String emailOK = "123@gamil.com";
+    String passwordOK = "1234";
+    String inputEmail = "";
+    String inputPassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +30,61 @@ public class MainActivity extends AppCompatActivity {
 
         //컴퍼런트 매칭 후 없는건 import
 
-        TextInputEditText_emil = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
         TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
         TextView_login = findViewById(R.id.TextView_login);
 
-        //1. 값을 가져온다
+        //1. 값을 가져온다 - 검사 (123@gamil.com / 1234)
         //2. 클릭을 감지한다
         //3. 1번의 값을 다음 엑티비티로 넘긴다
 
-        TextView_login.setClickable(true);
+        TextView_login.setClickable(false); //기본 로그인 비활성후 조건부 활성화
+        TextInputEditText_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null) {    //null 체크 로직
+                    Log.d("SENTI", s.toString());   //로그 확인용
+                    inputEmail = s.toString();
+                    TextView_login.setClickable(validation());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        TextInputEditText_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null) {
+                    Log.d("SENTI", s.toString());
+                    inputPassword = s.toString();
+                    TextView_login.setClickable(validation());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+//        TextView_login.setClickable(true);
         TextView_login.setOnClickListener(new View.OnClickListener() {  //클릭시 하고싶은 동작 작성
             @Override
             public void onClick(View v) {
-                String email = TextInputEditText_emil.getText().toString();
+                String email = TextInputEditText_email.getText().toString();
                 String password = TextInputEditText_password.getText().toString();
 
                 //String 으로 값 받고 MainActivity 에서 LoginResultActivity 로 넘기기
@@ -45,5 +95,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);  //intent 를 실행하라
             }
         });
+    }
+    public boolean validation(){    //검사 함수
+        return inputEmail.equals(emailOK)  && inputPassword.equals(passwordOK);  //자바에서 문자열 비교는 equals | == 는 사용 X
     }
 }
